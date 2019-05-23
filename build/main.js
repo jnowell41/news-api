01b2b51cf5e731f5985a58fda4d05d9643e863ed
James Nowell <Nowlie@JamesNoellsMBP4.lan>
2019-05-23 13:26:50 +0100

    formattedinputs new styling

--- build/main.js
+++ build/main.js
@@ -2,15 +2,33 @@ var APIkey = "376269d9f7e94faba6b258c8521c87c2";
 var imageChecker = function (url) {
     return url ? url : "https://picsum.photos/500/350";
 };
+var changeBbcSource = function (source) {
+    var convertedSource = "";
+    if (source.includes("https://www.facebook.com/bbcnews"))
+        return convertedSource = "BBC News";
+    else if (source.includes("https://www.facebook.com/DailyMailCeleb"))
+        return convertedSource = "Daily Mail";
+    else {
+        return source;
+    }
+};
+var myData = {};
+function convertDate(date) {
+    var array = [];
+    for (var i = 0; i < 10; i++) {
+        array.push(date.charAt(i));
+    }
+    return array.join("");
+}
 fetch("https://newsapi.org/v2/top-headlines?country=gb&apiKey=" + APIkey)
     .then(function (res) { return res.json(); })
     .then(function (data) {
-    var eachArticle = data.articles.map(function (element, index) { return (document.getElementsByClassName("container")[0].innerHTML += "<div key = " + index + " class = \"card\">\n    <div id = \"imageContainer\" onclick = " + showDescription + ">\n    <img src = " + imageChecker(element.urlToImage) + " />\n    </div>\n    <h1>" + (element.title ? element.title : "Unable to retrieve data") + "</h1>\n    <p id =\"authorWithDate\">\n    Published by " + (element.author ? element.author : "Unknown") + " <br/>\n    " + (element.publishedAt ? element.publishedAt : "") + "\n    </p>\n    <p id = \"description\">" + (element.description ? element.description : "Unable to retrieve data") + "</p>\n    </div>"); });
+    var eachArticle = data.articles.map(function (element, index) { return (document.getElementsByClassName("container")[0].innerHTML += "<div key = " + index + " class = \"card\">\n    <div id = \"imageContainer\" >\n    <img src = " + imageChecker(element.urlToImage) + " />\n    </div>\n    <h1>" + (element.title ? element.title : "Unable to retrieve data") + "</h1>\n    <p id =\"authorWithDate\">\n    Published by " + (element.author ? changeBbcSource(element.author) : "Unknown") + " <br/>\n    " + (element.publishedAt ? convertDate(element.publishedAt) : "") + "\n    </p>\n    <p id = \"description\">" + (element.description ? element.description : "Unable to retrieve data") + "</p>\n    </div>"); });
 });
+console.log(myData);
 //tracer bullet on input field value, needs to be passed as a filter for article contents 
-var showDescription = function () {
-    document.getElementById("description").classList.toggle("toggleDisplay");
-};
+var allCards = document.querySelectorAll(".card");
+console.log(document.getElementsByClassName(".card").length); // returns 0, why?
 function getInput(value) {
     console.log(value);
     return value;
--- main.ts
+++ main.ts
@@ -26,32 +26,70 @@ const imageChecker = (url) => {
     return url ? url : "https://picsum.photos/500/350"
 };
 
+const changeBbcSource = (source) => {
+    let convertedSource = "";
+    if (source.includes("https://www.facebook.com/bbcnews")) 
+     return convertedSource = "BBC News";
+    else if(source.includes("https://www.facebook.com/DailyMailCeleb"))
+    return convertedSource = "Daily Mail";
+     else {
+         return source;
+     }
+}
+
+let myData = {};
+
+function convertDate(date) {
+    let array = [];
+    for (let i=0;i<10;i++) {
+        array.push(date.charAt(i)
+    }
+        return array.join("");
+}
+
 fetch("https://newsapi.org/v2/top-headlines?country=gb&apiKey="+APIkey)
 .then(res => res.json())
-.then(data => {let eachArticle =  data.articles.map((element, index) => (
+.then(data => {let eachArticle =  data.articles.map((element, index) => ( 
     document.getElementsByClassName("container")[0].innerHTML += `<div key = ${index} class = "card">
-    <div id = "imageContainer" onclick = ${showDescription}>
+    <div id = "imageContainer" >
     <img src = ${imageChecker(element.urlToImage)} />
     </div>
     <h1>${element.title ? element.title : "Unable to retrieve data"}</h1>
     <p id ="authorWithDate">
-    Published by ${element.author ? element.author : "Unknown"} <br/>
-    ${element.publishedAt ? element.publishedAt : ""}
+    Published by ${element.author ? changeBbcSource(element.author) : "Unknown"} <br/>
+    ${element.publishedAt ? convertDate(element.publishedAt) : ""}
     </p>
     <p id = "description">${element.description ? element.description : "Unable to retrieve data"}</p>
     </div>`
 ))});
+console.log(myData);
 
 //tracer bullet on input field value, needs to be passed as a filter for article contents 
 
-const showDescription = () => {
-    document.getElementById("description").classList.toggle("toggleDisplay");
-}
+let allCards = document.querySelectorAll(".card");
+
+console.log(document.getElementsByClassName(".card").length) // returns 0, why?
 
 function getInput(value) {
     console.log(value);
     return value;
 };
+
+// function filterData(input) {
+//     if (input===JSON.stringify(allCards).charAt(input) {
+//         document.getElementsByClassName("container")[0].innerHTML += `<div key = ${index} class = "card">
+//         <div id = "imageContainer" >
+//         <img src = ${imageChecker(this.element.urlToImage)} />
+//         </div>
+//         <h1>${this.element.title ? this.element.title : "Unable to retrieve data"}</h1>
+//         <p id ="authorWithDate">
+//         Published by ${this.element.author ? this.element.author : "Unknown"} <br/>
+//         ${this.element.publishedAt ? this.element.publishedAt : ""}
+//         </p>
+//         <p id = "description">${this.element.description ? this.element.description : "Unable to retrieve data"}</p>
+//         </div>`
+//     }
+}
 
 
 var container = document.getElementsByClassName("container");
