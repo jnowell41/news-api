d26d52b599a9fb6b24146b127c06418f664056a2
James Nowell <Nowlie@JamesNoellsMBP4.lan>
2019-05-23 11:20:31 +0100

    implemented grid display

--- build/main.js
+++ build/main.js
@@ -1,10 +1,21 @@
 var APIkey = "376269d9f7e94faba6b258c8521c87c2";
+var imageChecker = function (url) {
+    return url ? url : "https://picsum.photos/500/350";
+};
 fetch("https://newsapi.org/v2/top-headlines?country=gb&apiKey=" + APIkey)
     .then(function (res) { return res.json(); })
-    .then(function (data) { return data.articles.map(function (element, index) { return (document.getElementsByClassName("container")[0].innerHTML += "<div key = " + index + " class = \"card\">\n    <div id = \"imageContainer\">\n    <img src = " + element.urlToImage + " />\n    </div>\n    <h1>" + element.title + "</h1>\n    <p id =\"authorWithDate\">\n    Published by " + element.author + " <br/>\n    " + element.publishedAt + "\n    </p>\n    <p id = \"description\">" + element.description + "</p>\n    </div>"); }); });
+    .then(function (data) {
+    var eachArticle = data.articles.map(function (element, index) { return (document.getElementsByClassName("container")[0].innerHTML += "<div key = " + index + " class = \"card\">\n    <div id = \"imageContainer\" onclick = " + showDescription + ">\n    <img src = " + imageChecker(element.urlToImage) + " />\n    </div>\n    <h1>" + (element.title ? element.title : "Unable to retrieve data") + "</h1>\n    <p id =\"authorWithDate\">\n    Published by " + (element.author ? element.author : "Unknown") + " <br/>\n    " + (element.publishedAt ? element.publishedAt : "") + "\n    </p>\n    <p id = \"description\">" + (element.description ? element.description : "Unable to retrieve data") + "</p>\n    </div>"); });
+});
+//tracer bullet on input field value, needs to be passed as a filter for article contents 
+var showDescription = function () {
+    document.getElementById("description").classList.toggle("toggleDisplay");
+};
 function getInput(value) {
     console.log(value);
     return value;
 }
-var field = document.getElementById("SearchBar");
+;
+var container = document.getElementsByClassName("container");
+console.log(JSON.stringify(container)); //comes out as an empty object, why?
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -22,25 +22,37 @@ interface IItems {
     articles:IArticles[];
 }
 
+const imageChecker = (url) => {
+    return url ? url : "https://picsum.photos/500/350"
+};
+
 fetch("https://newsapi.org/v2/top-headlines?country=gb&apiKey="+APIkey)
 .then(res => res.json())
-.then(data => data.articles.map((element, index) => (
+.then(data => {let eachArticle =  data.articles.map((element, index) => (
     document.getElementsByClassName("container")[0].innerHTML += `<div key = ${index} class = "card">
-    <div id = "imageContainer">
-    <img src = ${element.urlToImage} />
+    <div id = "imageContainer" onclick = ${showDescription}>
+    <img src = ${imageChecker(element.urlToImage)} />
     </div>
-    <h1>${element.title}</h1>
+    <h1>${element.title ? element.title : "Unable to retrieve data"}</h1>
     <p id ="authorWithDate">
-    Published by ${element.author} <br/>
-    ${element.publishedAt}
+    Published by ${element.author ? element.author : "Unknown"} <br/>
+    ${element.publishedAt ? element.publishedAt : ""}
     </p>
-    <p id = "description">${element.description}</p>
+    <p id = "description">${element.description ? element.description : "Unable to retrieve data"}</p>
     </div>`
-)));
+))});
+
+//tracer bullet on input field value, needs to be passed as a filter for article contents 
+
+const showDescription = () => {
+    document.getElementById("description").classList.toggle("toggleDisplay");
+}
 
 function getInput(value) {
     console.log(value);
     return value;
-}
+};
 
-var field:HTMLElement = document.getElementById("SearchBar");
+
+var container = document.getElementsByClassName("container");
+console.log(JSON.stringify(container)); //comes out as an empty object, why?
